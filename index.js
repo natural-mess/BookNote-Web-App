8cb11188661bb586d528e004512c4753b566e054
natural-mess <37369211+natural-mess@users.noreply.github.com>
2025-08-05 11:06:30 +0200

    Finish sorting

--- index.js
+++ index.js
@@ -40,21 +40,24 @@ var sortType = 1;
 
 // Get data from db
 app.get("/", async (req, res) => {
+  const sortType = req.query.sort;
   var result;
   try {
     switch (sortType) {
-      case 2:
-        result = await db.query("SELECT * FROM books ORDER BY date ASC");
+      case "recency":
+        result = await db.query("SELECT * FROM books ORDER BY date DESC");
         break;
 
-      case 3:
+      case "rating":
         result = await db.query("SELECT * FROM books ORDER BY rating DESC");
         break;
 
-      case 1:
-        result = await db.query("SELECT * FROM books ORDER BY id ASC");
-      default:
+      case "title":
+        result = await db.query("SELECT * FROM books ORDER BY name ASC");
         break;
+
+      default:
+        result = await db.query("SELECT * FROM books ORDER BY id ASC"); // default order
     }
     books = result.rows;
     res.render("index.ejs", {
@@ -66,15 +69,15 @@ app.get("/", async (req, res) => {
 });
 
 // Sorting notes
-app.post("/", async (req, res) => {
-  console.log(req.body);
+app.get("/book", async (req, res) => {
+  console.log(req.query);
 });
 
 // Create new book note
 app.get("/new", (req, res) => {
   res.render("new.ejs", {
     heading: "New Note",
-    submit: "Create Note"
+    submit: "Create Note",
   });
 });
 
@@ -87,7 +90,8 @@ app.post("/new", async (req, res) => {
   var rating = req.body.rating;
   var note = req.body.note;
   try {
-    await db.query("INSERT INTO books (isbn, name, author, rating, note) VALUES ($1, $2, $3, $4, $5)",
+    await db.query(
+      "INSERT INTO books (isbn, name, author, rating, note) VALUES ($1, $2, $3, $4, $5)",
       [isbn, name, author, rating, note]
     );
     res.redirect("/");
@@ -99,10 +103,12 @@ app.post("/new", async (req, res) => {
 // View book note in detail
 app.get("/view/:id", async (req, res) => {
   try {
-    const result = await db.query("SELECT * FROM books WHERE id = $1", [req.params.id]);
+    const result = await db.query("SELECT * FROM books WHERE id = $1", [
+      req.params.id,
+    ]);
     var book = result.rows[0];
     res.render("view.ejs", {
-      book
+      book,
     });
   } catch (err) {
     console.log(err);
@@ -112,12 +118,14 @@ app.get("/view/:id", async (req, res) => {
 // Fetch current data from selected book note for editing
 app.get("/edit/:id", async (req, res) => {
   try {
-    const result = await db.query("SELECT * FROM books WHERE id = $1", [req.params.id]);
+    const result = await db.query("SELECT * FROM books WHERE id = $1", [
+      req.params.id,
+    ]);
     var book = result.rows[0];
     res.render("new.ejs", {
       book,
       heading: "Edit Note",
-      submit: "Update Note"
+      submit: "Update Note",
     });
   } catch (err) {
     console.log(err);
@@ -133,7 +141,8 @@ app.post("/edit/:id", async (req, res) => {
   var note = req.body.note;
   var bookId = req.params.id;
   try {
-    await db.query("UPDATE books SET isbn = $1, name = $2, author = $3, rating = $4, note = $5, updated_at = NOW() WHERE id = $6;",
+    await db.query(
+      "UPDATE books SET isbn = $1, name = $2, author = $3, rating = $4, note = $5, updated_at = NOW() WHERE id = $6;",
       [isbn, name, author, rating, note, bookId]
     );
     res.redirect("/");
@@ -144,14 +153,16 @@ app.post("/edit/:id", async (req, res) => {
 
 // Confirmation before delete a note
 app.get("/delete/:id", async (req, res) => {
-  var bookId =req.params.id;
+  var bookId = req.params.id;
   try {
-    const result = await db.query("SELECT name FROM books WHERE id = $1", [bookId]);
+    const result = await db.query("SELECT name FROM books WHERE id = $1", [
+      bookId,
+    ]);
     var bookName = result.rows[0].name;
     res.render("delete.ejs", {
       bookName,
       bookId,
-      heading: "Delete Note"
+      heading: "Delete Note",
     });
   } catch (err) {
     console.log(err);
@@ -160,7 +171,7 @@ app.get("/delete/:id", async (req, res) => {
 
 // Delete a note from database if user confirms
 app.post("/delete/:id", async (req, res) => {
-  var bookId =req.params.id;
+  var bookId = req.params.id;
   try {
     await db.query("DELETE FROM books WHERE id = $1", [bookId]);
     res.redirect("/");
